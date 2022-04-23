12301a5f1b7074dc4635279553f740b37660ecd7
walt <2963975+waltiam@users.noreply.github.com>
2022-04-23 08:57:10 -0400

    some functionality

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MySql.Data.MySqlClient;
+using Dapper;
+
+var builder = new MySqlConnectionStringBuilder
+{
+    Server = "db",
+    UserID = "root",
+    Password = "WOOFERS",
+    Database = "sample",
+};
+
+// open a connection asynchronously
+await using var connection = new MySqlConnection(builder.ConnectionString);
+var insert = "insert into records (name) values (@name)";
+// var name = Guid.NewGuid();//.ToString("N") ;
+// var record = new { name = "Mic" };
+
+await connection.ExecuteAsync(insert, new { name = "Pete" });
+
